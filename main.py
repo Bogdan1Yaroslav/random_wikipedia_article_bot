aa13699e7fa569ceb2998bdbe8b0bbb2686d68ea
Bogdan1Yaroslav <bogdan.yaroslav.1997@bk.ru>
2022-07-29 22:49:06 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import logging
+from aiogram import executor, types
+from bot_settings import dp
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
